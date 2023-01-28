9cdc8050d56eae768ab82c48d2ebbfe3faf8c757
Igor <igorlfs@ufmg.br>
2023-01-28 20:28:58 -0300

    chore(main): resize and rename

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ mod ui;
 
 fn main() {
     let native_options = eframe::NativeOptions {
-        initial_window_size: Some(Vec2::new(990., 700.)),
+        initial_window_size: Some(Vec2::new(990., 710.)),
         ..Default::default()
     };
     eframe::run_native(
-        "My egui App",
+        "Visualizer",
         native_options,
         Box::new(|cc| Box::new(ui::Visualizer::new(cc))),
     );
